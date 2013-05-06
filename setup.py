b010548e0ca806170732849ed5f3e64b92d021d4
Mihnea Simian <contact@mesimian.com>
2013-05-06 11:41:15 +0300

    fixed dashed lines

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='sanap',
-    version='0.1',
+    version='0.2',
     url='https://github.com/eaudeweb/eea.sanap',
     license='Mozilla Public License',
     author='Eau de Web',
